b678eab687198b0af00972f41411b7bd4e829148
ake <ake@michael-2.local>
2016-03-13 20:21:55 +0800

    新建网易新闻项目

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by ake on 16/3/13.
+//  Copyright © 2016年 ake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
